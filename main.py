3ba148d7ab1d2912b682415e3c61fad9d6c649b7
archeltaneka <archeltaneka@ymail.com>
2020-02-03 18:13:50 +0000

    added debugging feature

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from tensorflow.keras.datasets import mnist, fashion_mnist
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 # define convolution, pooling, and fully connected(dense) layers
-convolution = Convolution2D(filter_shape=(3,3), num_filters=8, padding='same', stride=1, kernel_init='none')
-maxpool = MaxPool2D(pool_size=2)
+convolution = Convolution2D(filter_shape=(3,3), num_filters=8, padding='same', stride=1, kernel_init='none', debugging=False)
+maxpool = MaxPool2D(pool_size=2, debugging=False)
 temp = convolution.conv2d(x_train[0]/255) # save img dimensions
 temp = maxpool.pool(temp)
 tmp_shape = temp.shape
-softmax = Softmax(tmp_shape[0]*tmp_shape[1]*tmp_shape[2], 10, activation='softmax')
+softmax = Softmax(tmp_shape[0]*tmp_shape[1]*tmp_shape[2], 10, activation='softmax', debugging=False)
 
 # train the model
 l, a = train(convolution, maxpool, softmax, epochs=5, lr=0.01, features=x_train[:1000], labels=y_train[:1000])
