f41e0a5b5e012ba3a714009c1ea4daa33242eeeb
archeltaneka <archeltaneka@ymail.com>
2020-02-03 15:38:38 +0000

    added flowtensor.py, minor changes on utils.py and main.py

--- main.py
+++ main.py
@@ -1,87 +1,25 @@
-from convolution import Convolution2D
-from pooling import MaxPool2D
-from fully_connected import Softmax
-
 import numpy as np
-from tensorflow.keras.datasets import mnist
+# import our custom library
+from flowtensor import Convolution2D, MaxPool2D, Softmax
+from utils import train, test, show_loss_graph, show_acc_graph
+# let's test with mnist datasets
+from tensorflow.keras.datasets import mnist, fashion_mnist
 
-# load data
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# load the dataset
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-# define the model
-convolution = Convolution2D(filter_shape=(3,3), num_filters=8, padding='same', stride=1)
+# define convolution, pooling, and fully connected(dense) layers
+convolution = Convolution2D(filter_shape=(3,3), num_filters=8, padding='same', stride=1, kernel_init='none')
 maxpool = MaxPool2D(pool_size=2)
-temp = convolution.conv2d(x_train[0]/255)
+temp = convolution.conv2d(x_train[0]/255) # save img dimensions
 temp = maxpool.pool(temp)
-temp_size = temp.shape
-softmax = Softmax(temp_size[0]*temp_size[1]*temp_size[2], 10, activation='softmax')
+tmp_shape = temp.shape
+softmax = Softmax(tmp_shape[0]*tmp_shape[1]*tmp_shape[2], 10, activation='softmax')
 
-def build_model(image):
-    # initialize the architecture
-    model = convolution.conv2d(image/255)
-    model = maxpool.pool(model)
-    model = softmax.dense(model)
-    
-    return model
-
-def compile_model(img, label, learning_rate):
-    
-    # initialize model
-    train_model = build_model(img)
-
-    # forward propagation step
-    output, loss, acc = softmax.forward_propagation(img, label, train_model)
-    
-    # initial gradient
-    grad = np.zeros(10) # 10 different classes
-    grad[label] = -1 / output[label]
-    
-    # back propagation
-    grad = softmax.back_propagation(grad, learning_rate)
-    grad = maxpool.back_propagation(grad)
-    grad = convolution.back_propagation(grad, learning_rate)
-    
-    return loss, acc
-
-
-def train(epochs, lr, features, labels):
-    loss_dict = []
-    acc_dict = []
-    NUM_EPOCHS = epochs
-    learning_rate = lr
-
-    for epoch in range(NUM_EPOCHS):  
-        print("============= EPOCH", epoch+1, "=============")
-        total_loss = 0
-        accuracy = 0
-
-        for i, (img, label) in enumerate(zip(features, labels)): # let's train first 1000 data for simplicity 
-            # build the complete model
-            if i % 100 == 0 and i != 0:
-                print("Step", i, ": Loss= ", total_loss/100, "| Accuracy=", accuracy, "%")
-
-                total_loss = 0
-                accuracy = 0
-            loss, acc = compile_model(img, label, learning_rate)
-            total_loss += loss
-            accuracy += acc
-            
-        loss_dict.append(total_loss)
-        acc_dict.append(accuracy)
-    
-    return loss_dict, acc_dict
-
-def test(X_test, y_test):
-    loss = 0
-    accuracy = 0
-    for img, label in zip(X_test, y_test):
-        test_model = build_model(img)
-        _, l, acc = softmax.forward_propagation(img, label, test_model, reg_lambda=1e-3)
-        loss += l
-        accuracy += acc
-
-    print("Test Loss: ", loss/len(X_test))
-    print("Test Accuracy: ", accuracy/len(y_test))
-    
-
-l, a = train(epochs=3, lr=0.01, features=x_train[:1000], labels=y_train[:1000])
+# train the model
+l, a = train(convolution, maxpool, softmax, epochs=5, lr=0.01, features=x_train[:1000], labels=y_train[:1000])
+# test the model
+preds = test(convolution, maxpool, softmax, x_test[:1000], y_test[:1000])
+# shows loss & accuracy graph
+show_loss_graph(l)
+show_acc_graph(a)
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
 
 def padding(img, pad):
     img_pad = np.pad(img, ((pad, pad), (pad,pad)), 'constant', constant_values=0)
@@ -10,35 +10,31 @@ def load_data():
     (x1, y1), (x2, y2) = mnist.load_data()
     return (x1, y1), (x2, y2)
 
-def build_model(image):
-    # initialize the architecture
-    model = convolution.conv2d(image/255)
-    model = maxpool.pool(model)
-    model = softmax.dense(model)
+def build_model(c, m, s, img):
+    train_model = c.conv2d(img/255)
+    train_model = m.pool(train_model)
+    train_model = s.dense(train_model)
     
-    return model
+    return train_model
 
-def compile_model(img, label, learning_rate):
-    
-    # initialize model
-    train_model = build_model(img)
-
+def compile_model(c, m, s, img, label, learning_rate):
+    model = build_model(c, m, s, img)
     # forward propagation step
-    output, loss, acc = softmax.forward_propagation(img, label, train_model)
+    output, loss, acc = s.forward_propagation(img, label, model)
     
     # initial gradient
     grad = np.zeros(10) # 10 different classes
     grad[label] = -1 / output[label]
     
     # back propagation
-    grad = softmax.back_propagation(grad, learning_rate)
-    grad = maxpool.back_propagation(grad)
-    grad = convolution.back_propagation(grad, learning_rate)
+    grad = s.back_propagation(grad, learning_rate)
+    grad = m.back_propagation(grad)
+    grad = c.back_propagation(grad, learning_rate)
     
     return loss, acc
 
 
-def train(epochs, lr, features, labels):
+def train(c, m, s, epochs, lr, features, labels):
     loss_dict = []
     acc_dict = []
     NUM_EPOCHS = epochs
@@ -52,11 +48,11 @@ def train(epochs, lr, features, labels):
         for i, (img, label) in enumerate(zip(features, labels)): # let's train first 1000 data for simplicity 
             # build the complete model
             if i % 100 == 0 and i != 0:
-                print("Step", i, ": Loss= ", total_loss/100, "| Accuracy=", accuracy, "%")
+                print("Step", i, ": Loss=", total_loss/100, "| Accuracy=", accuracy, "%")
 
                 total_loss = 0
                 accuracy = 0
-            loss, acc = compile_model(img, label, learning_rate)
+            loss, acc = compile_model(c, m, s, img, label, learning_rate)
             total_loss += loss
             accuracy += acc
             
@@ -65,12 +61,12 @@ def train(epochs, lr, features, labels):
     
     return loss_dict, acc_dict
 
-def test(X_test, y_test):
+def test(c, m, s, X_test, y_test):
     loss = 0
     accuracy = 0
     for img, label in zip(X_test, y_test):
-        test_model = build_model(img)
-        _, l, acc = softmax.forward_propagation(img, label, test_model, reg_lambda=1e-3)
+        test_model = build_model(c, m, s, img)
+        _, l, acc = s.forward_propagation(img, label, test_model, reg_lambda=1e-3)
         loss += l
         accuracy += acc
 
